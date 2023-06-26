64c1493a00a34170d257606135999bde75e5ef4c
silva4dev <web4dev.lucas@outlook.com>
2023-06-25 23:19:32 -0300

    chore: change register controller

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,29 +1,36 @@
-import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
-import { RegisterUseCase } from "@/use-cases/register";
-import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
-  });
+  })
 
-  const { name, email, password } = registerBodySchema.parse(request.body);
+  const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
 
     await registerUseCase.execute({
       name,
       email,
       password,
-    });
+    })
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: error.message
+      })
+    }
+
+    throw error
   }
 
-  return reply.status(201).send();
+  return reply.status(201).send()
 }
