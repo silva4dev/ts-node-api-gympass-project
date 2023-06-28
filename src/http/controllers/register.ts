656546fdb3aec0fe05e99fe41686c69592d367df
silva4dev <web4dev.lucas@outlook.com>
2023-06-27 21:40:55 -0300

    chore: change register controller

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -14,8 +14,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({
       name,
