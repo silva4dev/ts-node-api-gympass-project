269f86e4e78bb05f9d42410a812327ef1a7f4d88
silva4dev <web4dev.lucas@outlook.com>
2023-06-29 19:45:20 -0300

    chore: change in memory users repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,11 +1,12 @@
-import { Prisma, User } from "@prisma/client";
-import { UsersRepository } from "../users-repository";
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
   async findById(id: string): Promise<User | null> {
-    const user = this.items.find(item => item.id === id)
+    const user = this.items.find((item) => item.id === id)
 
     if (!user) {
       return null
@@ -14,9 +15,8 @@ export class InMemoryUsersRepository implements UsersRepository {
     return user
   }
 
-
   async findByEmail(email: string): Promise<User | null> {
-    const user = this.items.find(item => item.email === email)
+    const user = this.items.find((item) => item.email === email)
 
     if (!user) {
       return null
@@ -27,7 +27,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
