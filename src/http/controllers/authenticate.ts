9bb9951f2d6b818228a9a2e02ec4ca0aad7ab6a7
silva4dev <web4dev.lucas@outlook.com>
2023-06-29 19:43:59 -0300

    chore: change authenticate controller

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -3,7 +3,10 @@ import { z } from 'zod'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
-export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
   const authenticateBodySchema = z.object({
     email: z.string().email(),
     password: z.string().min(6),
@@ -21,7 +24,7 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({
-        message: error.message
+        message: error.message,
       })
     }
 
