9dfb4f51796081391242f2dacd9e2da1e326501c
silva4dev <web4dev.lucas@outlook.com>
2023-06-29 19:45:31 -0300

    chore: change authenticate spec

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -29,7 +29,7 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong email', async () => {
-    expect(() => sut.execute({
+    await expect(() => sut.execute({
       email: 'johndoe@example.com',
       password: '123456'
     })).rejects.toBeInstanceOf(InvalidCredentialsError)
@@ -42,7 +42,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('123456', 6)
     })
 
-    expect(() => sut.execute({
+    await expect(() => sut.execute({
       email: 'johndoe@example.com',
       password: '123123'
     })).rejects.toBeInstanceOf(InvalidCredentialsError)
