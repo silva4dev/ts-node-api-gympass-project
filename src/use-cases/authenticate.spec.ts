2f8a66f508a97646ec577dd59bfeb9ec847e7d27
silva4dev <web4dev.lucas@outlook.com>
2023-07-09 02:27:28 -0300

    chore: change authenticate spec

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -17,34 +17,38 @@ describe('Authenticate Use Case', () => {
     await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
-      password_hash: await hash('123456', 6)
+      password_hash: await hash('123456', 6),
     })
 
     const { user } = await sut.execute({
       email: 'johndoe@example.com',
-      password: '123456'
+      password: '123456',
     })
 
     expect(user.id).toEqual(expect.any(String))
   })
 
   it('should not be able to authenticate with wrong email', async () => {
-    await expect(() => sut.execute({
-      email: 'johndoe@example.com',
-      password: '123456'
-    })).rejects.toBeInstanceOf(InvalidCredentialsError)
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 
   it('should not be able to authenticate with wrong password', async () => {
     await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
-      password_hash: await hash('123456', 6)
+      password_hash: await hash('123456', 6),
     })
 
-    await expect(() => sut.execute({
-      email: 'johndoe@example.com',
-      password: '123123'
-    })).rejects.toBeInstanceOf(InvalidCredentialsError)
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
